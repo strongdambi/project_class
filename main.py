af389ed7a8d8f08c76c0057fa69f81321a313c58
eddie <eddie@eddies-MacBook-Air.local>
2024-07-05 16:25:51 +0900

    finish input func

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ class Member:
         self.name = name
         self.username = username
         self.password = password
-
     def display(self):
         print(f"이름: {self.name}, ID: {self.username}")
         pass
@@ -60,19 +59,89 @@ members.append(m3)
 #         print(post.title)
 
 # ----- input을 이용하여 Member, Post 인스턴스 생성 ------
+while True:
+    print("\n[작업 목록]\n1. 계정 추가\n2. 유저 목록\n3. 게시글 목록\n4. 게시글 작성\n5. 게시글 삭제\n그외. 종료") # 작업 5를 추가해 글 삭제 기능을 구현
+    select = input("원하는 작업을 선택하세요: ")
+    if select == '1': # 계정 생성
+        print("\n[계정 생성]")
+        name = input("이름: ")
+        username = input("ID: ")
+        password = input("PS: ")
+        members.append(Member(name, username, password))
+        print("계정 생성 완료!")
 
-name = input("이름을 입력하세요: ")
-username = input("ID를 입력하세요: ")
-password = input("비밀번호를 입력하세요: ")
-members.append(Member(name, username, password))
+    elif select == '2':  # 유저 목록 확인
+        print("\n[유저 목록]")
+        user_table = []
+        for member in members:
+            user_table.append([member.name, member.username])
+        print(tabulate(user_table, headers=['Name', 'ID'], tablefmt='fancy_grid'))
 
-member_lists = [[member.name, member.username] for member in members]
-print(tabulate(member_lists, headers=["Name", "ID"], tablefmt='fancy_grid'))
+    elif select == '3': # 글 목록 확인
+        post_table = []
+        for post in posts:
+            post_table.append([post.title, post.author.username, post.content])
+        print(tabulate(post_table, headers=['Title','Author','Contnet'], tablefmt='fancy_grid'))
+        post_search = input("키워드를 입력하세요. ")
+        print("\n키워드를 포함한 게시글 입니다.")
+        post_search_table = []
+        for post in posts:
+            if post_search in post.content:
+                post_search_table.append([post.title, post.author.username, post.content])
+                print(tabulate(post_search_table, headers=['Title','Author','Contnet'], tablefmt='fancy_grid'))
 
-title = input("제목을 입력하세요: ")
-content = input("내용을 입력하세요: ")
-author = input("아이디를 입력하세요: ")
-posts.append(Post(title, content, author))
+    elif select == '4': # 게시글 작성
+        while True:
+            id_input = input("ID를 입력해주세요. ")
+            member_matched = None
+            for member in members:
+                if id_input == member.username:
+                    member_matched = member
+                    break
+            if member_matched:
+                while True:
+                    ps_input = input("패스워드를 입력해주세요. ")
+                    if ps_input == member_matched.password:
+                        print("게시글을 작성해주세요.")
+                        title = input("\n제목: ")
+                        content = input("내용: ")
+                        posts.append(Post(title, content, member_matched))
+                        print("\n게시글 작성을 완료했습니다.")
+                        break
+                    else:
+                        print("패스워드가 일치하지 않습니다.")
+                break
+            else:
+                print("ID가 존재하지 않습니다. ")
+
+    elif select == '5':  # 게시글 삭제
+        del_title = input("삭제할 게시글의 제목을 입력하세요. ")
+        post_matched = None
+        for post in posts:
+            if del_title == post.title:
+                post_matched = post
+                break
+        if post_matched:
+            while True:
+                id_input = input("ID를 입력해주세요. ")
+                if id_input == post_matched.author.username:
+                    while True:
+                        ps_input = input("패스워드를 입력해주세요. ")
+                        if ps_input == post_matched.author.password:
+                            posts.remove(post_matched)
+                            print(f'{post_matched.title}이 삭제되었습니다.')
+                            break
+                        else:
+                            print("패스워드가 일치하지 않습니다.")
+                    break
+                else:
+                    print("아이디가 일치하지 않습니다.")
+        else:
+            print("삭제할 게시글이 없습니다.")
+
+    else: # 위에 없는 내용이 입력될 시 종료
+        print("종료합니다.")
+        break
 
 # # 여기서부터 아래 끝까지 드래그 한 후 ctrl+/ 하고 보시면 잘 보입니다!!
 #
