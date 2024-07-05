7c14e6da7e61bca5359370823b96615f75d38455
Drills-hub <sdi6566@gmail.com>
2024-07-05 13:04:12 +0900

    post-tabulate 기능추가

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ class Member:
         self.password = password
 
     def display(self):
-        print(f"이름: {self.name}, ID: {self.username}")
+        print(f"Name: {self.name}, ID: {self.username}")
+        pass
 
 class Post:
+
     def __init__(self, title, content, author):
         self.title = title
         self.content = content
         self.author = author
 
-    def read(cls):
-        print(f"글 정보")
-        print(tabulate(posts, headers='firstrow', tablefmt='simple_grid'))
+    def check(self):
+        print(f"Title: {self.title}, Content: {self.content}, Author:{self.author}" )
+
+
+
+
+
 
 
 # ----- 코드 실행 ------
@@ -25,23 +31,23 @@ members = []
 posts = []
 
 # # ----- 인스턴스 ------
-# m1 = Member("이름1", "아이디1", "비밀번호1")
-# m2 = Member("이름2", "아이디2", "비밀번호2")
-# m3 = Member("이름3", "아이디3", "비밀번호3")
+m1 = Member("Alice", "alt", "비밀번호1")
+m2 = Member("bob", "uncle1", "비밀번호2")
+m3 = Member("jay", "jet5", "비밀번호3")
 
-p1 = Post("bg_game", "game", "아이디1")
-p2 = Post("tekken_game", "game", "아이디2")
-p3 = Post("python", "study", "아이디3")
-p4 = Post("out", "movie", "아이디1")
-p5 = Post("21", "movie", "아이디2")
-p6 = Post("flask", "study", "아이디3")
-p7 = Post("basketball", "sports", "아이디1")
-p8 = Post("baseball", "sports", "아이디2")
-p9 = Post("ping-pong", "sports", "아이디3")
+p1 = Post("bg_game", "game is good ", "alt")
+p2 = Post("tekken_game", "game is bad", "uncle1")
+p3 = Post("python", "study is easy", "jet5")
+p4 = Post("out", "movie is bad", "alt")
+p5 = Post("21", "movie is soso", "uncle1")
+p6 = Post("flask", "study is so difficult", "jet5")
+p7 = Post("basketball", "sports is hard", "alt")
+p8 = Post("baseball", "sports is cool", "uncle1")
+p9 = Post("ping-pong", "sports is funny ", "jet5")
 #
-# members.append(m1)
-# members.append(m2)
-# members.append(m3)
+members.append(m1)
+members.append(m2)
+members.append(m3)
 posts.append(p1)
 posts.append(p2)
 posts.append(p3)
@@ -51,10 +57,6 @@ posts.append(p6)
 posts.append(p7)
 posts.append(p8)
 posts.append(p9)
-
-Post.read()
-
-
 #
 # for post in posts:
 #     if post.author == '아이디1':
@@ -64,18 +66,25 @@ Post.read()
 # for post in posts:
 #     if word in post.content:
 #         print(post.title)
-
 # ----- input을 이용하여 Member, Post 인스턴스 생성 ------
 
-name = input("이름을 입력하세요: ")
-username = input("ID를 입력하세요: ")
-password = input("비밀번호를 입력하세요: ")
-members.append(Member(name, username, password))
+# name = input("이름을 입력하세요: ")
+# username = input("ID를 입력하세요: ")
+# password = input("비밀번호를 입력하세요: ")
+# members.append(Member(name, username, password))
 
-title = input("제목을 입력하세요: ")
-content = input("내용을 입력하세요: ")
-author = input("아이디를 입력하세요: ")
-posts.append(Post(title, content, author))
+member_lists = [[member.name, member.username] for member in members]
+print(f"유저 목록")
+print(tabulate(member_lists,stralign='center', headers=["Name", "ID"], tablefmt='fancy_grid'))
+
+# title = input("제목을 입력하세요: ")
+# content = input("내용을 입력하세요: ")
+# author = input("아이디를 입력하세요: ")
+# posts.append(Post(title, content, author))
+
+post_lists = [[post.title,post.content,post.author] for post in posts]
+print(f"글 목록")
+print(tabulate(post_lists,stralign='center',headers=["Title", "Content","Author"],tablefmt='fancy_grid'))
 
 # # 여기서부터 아래 끝까지 드래그 한 후 ctrl+/ 하고 보시면 잘 보입니다!!
 #
