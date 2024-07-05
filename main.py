3a69904ac6129d7aee85a14c4209bf181fc87408
strongdambi <thekeytohong@gmail.com>
2024-07-05 09:18:25 +0900

    commit before pull

--- main.py
+++ main.py
@@ -22,52 +22,58 @@ members = []
 posts = []
 
 # # ----- 인스턴스 ------
-# m1 = Member("이름1", "아이디1", "비밀번호1")
-# m2 = Member("이름2", "아이디2", "비밀번호2")
-# m3 = Member("이름3", "아이디3", "비밀번호3")
-# p1 = Post("제목1", "내용1", "아이디1")
-# p2 = Post("제목2", "내용2", "아이디2")
-# p3 = Post("제목3", "내용3", "아이디3")
-# p4 = Post("제목4", "내용4", "아이디4")
-# p5 = Post("제목5", "내용5", "아이디5")
-# p6 = Post("제목6", "내용6", "아이디6")
-# p7 = Post("제목7", "내용7", "아이디7")
-# p8 = Post("제목8", "내용8", "아이디8")
-# p9 = Post("제목9", "내용9", "아이디9")
-#
-# members.append(m1)
-# members.append(m2)
-# members.append(m3)
-# posts.append(p1)
-# posts.append(p2)
-# posts.append(p3)
-# posts.append(p4)
-# posts.append(p5)
-# posts.append(p6)
-# posts.append(p7)
-# posts.append(p8)
-# posts.append(p9)
-#
-# for post in posts:
-#     if post.author == '아이디1':
-#         print(post.title)
-#
-# word = input("단어 입력: ")
-# for post in posts:
-#     if word in post.content:
-#         print(post.title)
+m1 = Member("이름1", "아이디1", "비밀번호1")
+m2 = Member("이름2", "아이디2", "비밀번호2")
+m3 = Member("이름3", "아이디3", "비밀번호3")
+p1 = Post("제목1", "내용1", "아이디1")
+p2 = Post("제목2", "내용2", "아이디2")
+p3 = Post("제목3", "내용3", "아이디3")
+p4 = Post("제목4", "내용4", "아이디4")
+p5 = Post("제목5", "내용5", "아이디5")
+p6 = Post("제목6", "내용6", "아이디6")
+p7 = Post("제목7", "내용7", "아이디7")
+p8 = Post("제목8", "내용8", "아이디8")
+p9 = Post("제목9", "내용9", "아이디9")
 
-# ----- input을 이용하여 Member, Post 인스턴스 생성 ------
+members.append(m1)
+members.append(m2)
+members.append(m3)
+posts.append(p1)
+posts.append(p2)
+posts.append(p3)
+posts.append(p4)
+posts.append(p5)
+posts.append(p6)
+posts.append(p7)
+posts.append(p8)
+posts.append(p9)
 
-name = input("이름을 입력하세요: ")
-username = input("ID를 입력하세요: ")
-password = input("비밀번호를 입력하세요: ")
-members.append(Member(name, username, password))
+# ----- members 리스트를 돌면서 회원들의 이름을 모두 프린트 ------
+for member in members:
+    member.display()
 
-title = input("제목을 입력하세요: ")
-content = input("내용을 입력하세요: ")
-author = input("아이디를 입력하세요: ")
-posts.append(Post(title, content, author))
+# ----- for 문을 돌면서 특정유저가 작성한 게시글의 제목을 모두 프린트 ------
+for post in posts:
+    if post.author == "아이디4":
+        print(post.title)
+
+# ----- for문을 돌면서 ‘특정 단어’가 content에 포함된 게시글의 제목을 모두 프린트 ------
+word = input("단어 입력: ")
+for post in posts:
+    if word in post.content:
+        print(post.author)
+
+# # ----- input을 이용하여 Member, Post 인스턴스 생성 ------
+#
+# name = input("이름을 입력하세요: ")
+# username = input("ID를 입력하세요: ")
+# password = input("비밀번호를 입력하세요: ")
+# members.append(Member(name, username, password))
+#
+# title = input("제목을 입력하세요: ")
+# content = input("내용을 입력하세요: ")
+# author = input("아이디를 입력하세요: ")
+# posts.append(Post(title, content, author))
 
 # # 여기서부터 아래 끝까지 드래그 한 후 ctrl+/ 하고 보시면 잘 보입니다!!
 #
